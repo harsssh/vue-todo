4d677c546009939cc61346a7058a12135f0d4071
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-05-18 01:52:02 +0900

    create todo table UI

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,52 +1,31 @@
 <template>
-  <div>Todo List</div>
-  <div class="todo-form">
-    <input
-      v-model="newItem.name"
-      class="todo-name"
-      placeholder="Todo"
-      type="text"
-    >
-    <input
-      v-model="newItem.deadline"
-      class="todo-deadline"
-      type="date"
-    >
-    <button
-      class="add-button"
-      type="button"
-      @click="addItem"
-    >
-      Add
-    </button>
-  </div>
+  <div>
+    <h2>Todo List</h2>
+    <div class="todo-form">
+      <input
+        v-model="newItem.name"
+        class="todo-name"
+        placeholder="Todo"
+        type="text"
+      />
+      <input v-model="newItem.deadline" class="todo-deadline" type="date" />
+      <button class="add-button" type="button" @click="addItem">Add</button>
+    </div>
 
-  <div class="item-list-container">
-    <ul class="item-list">
-      <li
-        v-for="item in items"
-        :key="item.name"
-      >
-        {{ item.name + ' : ' + item.deadline }}
-      </li>
-    </ul>
+    <TodoTable :items="items" class="todo-table" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import {reactive} from "vue";
+import { reactive } from 'vue'
+import TodoTable, { Todo } from './TodoTable.vue'
 
-interface Todo {
-  name: string,
-  deadline: string
-}
-
-const newItem = reactive<Todo>({name: '', deadline: ''})
-const items = reactive<Todo[]>([])
+const newItem = reactive<Todo>({ name: '', deadline: '' })
+const items = reactive<Todo[]>([{ name: 'sample', deadline: '2022-05-18' }])
 
 const addItem = () => {
   if (newItem.name != '' && newItem.deadline != '') {
-    items.push({name: newItem.name, deadline: newItem.deadline})
+    items.push({ name: newItem.name, deadline: newItem.deadline })
     newItem.name = ''
   }
 }
@@ -54,7 +33,9 @@ const addItem = () => {
 
 <style lang="scss" scoped>
 .todo-form {
-  .todo-name, .todo-deadline, .add-button {
+  .todo-name,
+  .todo-deadline,
+  .add-button {
     box-sizing: border-box;
     height: 30px;
     vertical-align: middle;
@@ -65,12 +46,8 @@ const addItem = () => {
   }
 }
 
-.item-list-container {
-  text-align: center;
-
-  .item-list {
-    display: inline-block;
-    text-align: left;
-  }
+.todo-table {
+  margin: 10px auto 0;
+  width: 90%;
 }
 </style>
---
+++ src/components/TodoTable.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th class="header-name">Name</th>
+          <th>Deadline</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in items" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.deadline }}</td>
+          <td>
+            <button class="delete-button">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+export interface Todo {
+  name: string
+  deadline: string
+}
+</script>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+defineProps<{ items: Todo[] }>()
+</script>
+
+<style lang="scss" scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+
+  tr {
+    border-bottom: solid 1px #ccc;
+  }
+
+  th,
+  td {
+    padding: 12px 0;
+  }
+
+  th.header-name {
+    width: 50%;
+  }
+
+  .delete-button {
+    $button-color: rgb(199, 80, 50);
+
+    padding: 10px;
+    border: none;
+    border-radius: 6px;
+    font-weight: bold;
+
+    background-color: $button-color;
+    color: #eee;
+    transition: background-color 0.2s;
+
+    cursor: pointer;
+
+    &:hover {
+      background-color: darken($button-color, 5%);
+    }
+  }
+}
+</style>
